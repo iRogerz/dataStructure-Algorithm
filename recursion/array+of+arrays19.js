7a511bab293f589d69c12c4f6d8fc17058db229f
iRogerz7 <roger.tseng@gtstw.net>
2025-10-02 11:43:39 +0800

    第四章(introduction to algorithm design)

---
+++ recursion/array+of+arrays19.js
@@ -0,0 +1,22 @@
+let arrs = [[[["a", [["b", ["c"]], ["d"]]], [["e"]], [[["f", "g", "h"]]]]]];
+
+test(arrs);
+console.log(result);
+
+function collector(arr1) {
+  let result = [];
+  helper(arr1);
+  return result;
+
+  function helper(arr2) {
+    for (let i = 0; i < arr2.length; i++) {
+      if (Array.isArray(arr2[i])) {
+        helper(arr2[i]);
+      } else {
+        result.push(arr2[i]);
+      }
+    }
+  }
+}
+
+console.log(collector(arrs));
---
+++ sliding_window/UniqueLetter15.js
@@ -0,0 +1,30 @@
+function uniqueLetterString(str) {
+  let start = 0;
+  let end = 0;
+  let counter = {};
+  let maxLength = -Infinity;
+
+  while (end < str.length) {
+    console.log(counter);
+    if (counter[str[end]]) {
+      counter[str[start]]--;
+      start++;
+    } else {
+      counter[str[end]] = 1;
+      end++;
+      if (end - start > maxLength) {
+        maxLength = end - start;
+      }
+    }
+  }
+
+  if (maxLength == -Infinity) {
+    console.log("Cannot find unique letters substring.");
+    return null;
+  }
+
+  console.log(maxLength);
+  return maxLength;
+}
+
+uniqueLetterString("thisishowwedoit"); // 6
